67f16eee157f4e0d25ab0f576c9a455bed0f781d
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-16 14:40:15 -0400

    Updated from Brython Server: 10/16/2017 2:40:15 PM

--- printnStars.py
+++ printnStars.py
@@ -1,1 +1,11 @@
+#Jack Robey
+#10/16/17
+#printnStars.py - takes an integer and prints out that many lines of stars
 
+def printnStars(num):
+    i = 1
+    while i <= num:
+        print('*'*i)
+        i += 1
+
+printnStars(5)
