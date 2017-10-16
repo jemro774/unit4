6c01946114fa408f08fccc7b02f2c4e57a14182b
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-16 14:45:44 -0400

    Updated from Brython Server: 10/16/2017 2:45:44 PM

--- countdownn.py
+++ countdownn.py
@@ -1,1 +1,10 @@
+#Jack Robey
+#10/16/17
+#countdownn.py - takes an integer as input and counts down to zero from that number
 
+def countdownn(num):
+    for i in range (num,0,-1):
+        print(i)
+    print('BOOM!')
+
+countdownn(5)
