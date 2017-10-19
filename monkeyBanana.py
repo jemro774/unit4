c9f2d41dda17bb80d8c6ac8bd5e745cd20af84ce
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-19 14:37:12 -0400

    Updated from Brython Server: 10/19/2017 2:37:11 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -3,6 +3,7 @@
 #monkeyBanana.py - best game ever
 
 from ggame import *
+from random import randint
 
 #constants
 ROWS = 20
@@ -25,6 +26,10 @@ def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
 
+def moveBanana():
+    banana.x = randint(0,COLS-1)*CELL_SIZE
+    banana.y = randint(0,ROWS-1)*CELL_SIZE
+
 if __name__ == '__main__':
     
     green = Color(0x006600,1)
@@ -36,7 +41,7 @@ if __name__ == '__main__':
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
     
     Sprite(jungleBox)
-    Sprite(bananaBox,(COLS*CELL_SIZE/2,ROWS*CELL_SIZE/2))
+    banana = Sprite(bananaBox,(COLS*CELL_SIZE/2,ROWS*CELL_SIZE/2))
     monkey = Sprite(monkeyBox)
     
     App().listenKeyEvent('keydown','right arrow',moveRight)
