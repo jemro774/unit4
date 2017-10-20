399c775021fc2c9165e34d159085e738cbe4a5c6
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-20 14:27:14 -0400

    Updated from Brython Server: 10/20/2017 2:27:12 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -10,6 +10,7 @@ ROWS = 20
 COLS = 20
 CELL_SIZE = 20
 
+#moves the monkey right
 def moveRight(event):
     if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x += CELL_SIZE
@@ -17,20 +18,23 @@ def moveRight(event):
             moveBanana()
             updateScore()
 
+#moves the monkey left
 def moveLeft(event):
     if monkey.x > 0:
         monkey.x -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#moves the monkey up   
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#moves the monkey down
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
@@ -38,20 +42,32 @@ def moveDown(event):
             moveBanana()
             updateScore()
 
+#moves the banana randomly
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
-    
+    data['frames'] = 0
+
+#updates score when monkey
 def updateScore():
     data['score'] += 5
     data['scoreText'].destroy()
     scoreBox = TextAsset('Score: '+str(data['score']))
     data['scoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
 
+#keeps track of how many frames have passed
+#moves banana if more than 300 passes
+def step():
+    data['frames'] += 1
+    if data['frames'] == 100:
+        moveBanana()
+
+#runs the game
 if __name__ == '__main__':
     
     data = {}
     data['score'] = 0
+    data['frames'] = 0
     
     green = Color(0x006600,1)
     brown = Color(0x8B4513,1)
@@ -71,4 +87,4 @@ if __name__ == '__main__':
     App().listenKeyEvent('keydown','left arrow',moveLeft)
     App().listenKeyEvent('keydown','up arrow',moveUp)
     App().listenKeyEvent('keydown','down arrow',moveDown)
-    App().run()
+    App().run(step)
