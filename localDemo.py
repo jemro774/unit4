def8b431a9c2d21de613fd190a7b90b43f22e540
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-23 14:18:14 -0400

    Updated from Brython Server: 10/23/2017 2:18:13 PM

--- localDemo.py
+++ localDemo.py
@@ -1,1 +1,12 @@
+#Jack Robey
+#10/23/17
+#localDemo.py - understanding local variables
 
+def f():
+    x = 77 #x is a local variable
+    y = 10 #y is also a local variable
+
+x = 5
+f() #x does not change
+print(x)
+print(y) #this will cause an error
