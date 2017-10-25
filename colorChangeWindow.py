6f5f216c47779dff4b77c2e91ad7c4c7cd461bb9
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-25 13:51:49 -0400

    Updated from Brython Server: 10/25/2017 1:51:48 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -2,6 +2,7 @@
 #10/20/17
 #colorChangeWindow.py - pops up a window that changes to a random color every time you click it
 
+from ggame import *
 from random import randint
 
 red = Color(0xff0000,1)
@@ -22,14 +23,30 @@ purpleOutline = LineStyle(1,purple)
 
 def mouseClick(event):
     num = randint(1,7)
-    if num = 1:
+    if num == 1:
         color = red
         outline = redOutline
-    elif num = 2:
+    elif num == 2:
         color = orange
-    elif num = 3:
+        outline = orangeOutline
+    elif num == 3:
         color = yellow
+        outline = yellowOutline
+    elif num == 4:
+        color = green
+        outline = greenOutline
+    elif num == 5:
+        color = lightBlue
+        outline = lightBlueOutline
+    elif num == 6:
+        color = blue
+        outline = blueOutline
+    else:
+        color = purple
+        outline = purpleOutline
     rectangle = RectangleAsset(1000,1000,outline,color)
-        
+    Sprite(rectangle)
+
 App().listenMouseEvent('click', mouseClick)
 App().run()
+
