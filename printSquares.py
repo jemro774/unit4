0a1bddc1a5153e49dda7093f1d51eb034b73b871
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-26 14:44:51 -0400

    Updated from Brython Server: 10/26/2017 2:44:49 PM

--- printSquares.py
+++ printSquares.py
@@ -1,3 +1,13 @@
 #Jack Robey
 #10/25/17
 #printSquares.py - prints out a grid of squares
+
+
+def printSquares(x,y):
+    for i in range(0,y):
+        print('+'+x*'--+')
+        print('|  '+x*'|  ')
+    print('+'+x*'--+')
+
+printSquares(5,6)
+
