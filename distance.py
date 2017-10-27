7c176cc9a7090ffb57814f488d27e0f17257dfb4
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-27 14:43:29 -0400

    Updated from Brython Server: 10/27/2017 2:43:27 PM

--- distance.py
+++ distance.py
@@ -3,6 +3,6 @@
 #distance.py - takes two points and returns the distance between the two points
 
 def distance(x1,y1,x2,y2):
-    return int((x2-x1)^2+(y2-y1)^2)^1/2)
+    return ((x2-x1)**2+(y2-y1)**2)**(1/2)
 
 print(distance(5,5,7,7))
