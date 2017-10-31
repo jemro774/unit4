18be9e4b350564977334f0d2a6d04d8ca555faa3
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-31 14:20:12 -0400

    Updated from Brython Server: 10/31/2017 2:20:12 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,27 @@
+#Jack Robey
+#10/31/17
+#quiz4.py
+
+def csia():
+    print("computer science is awesome")
+    print("computer science is awesome")
+    print("computer science is awesome")
+    print("computer science is awesome")
+    print("computer science is awesome")
+
+def average(x,y,z):
+    return (x+y+z)/3
+
+def lastLetter(name):
+    for ch in name:
+        print(ch)
+
+def same(a,b):
+    if a == b:
+        return True
+    else:
+        return False
+
+lastLetter('Jack')
+
 
